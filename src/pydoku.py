e631c3e2ef8e0ce34a3e81a74c98666af91e1df6
gonchi sanchez <gonchi.sanchez@gmail.com>
2012-03-08 07:53:51 -0300

    Cambio completamente la logica, anda el creador del sudoku, falta hacer andar el creador de puzzle.

--- src/pydoku.py
+++ src/pydoku.py
@@ -55,97 +55,125 @@ import random
 
 class Sudoku(object):
 
-    def __init__(self, tamanio):
-        """Inicia el board"""
-        self.board = [None for i in xrange(tamanio*tamanio)]
-        self.poblar_board()
-        #self.board = [1,2,3,4,5,6,7,8,9,4,5,6,7,8,9,1,2,3,7,8,9,1,2,3,4,5,6,2,3,4,5,6,7,8,9,1,5,6,7,8,9,1,2,3,4,8,9,1,2,3,4,5,6,7,3,4,5,6,7,8,9,1,2,6,7,8,9,1,2,3,4,5,9,1,2,3,4,5,6,7,8]
+    def __init__(self):
+        """Inicializa el board (tablero) vacio."""
+        self.limpiar_board()
 
-    def poblar_board(self):
-        self.poblar_celda(0)
+    def limpiar_board(self):
+        """Para indicar un casillero vacio, se utiliza la convencion del foro,
+        donde se explicita que un casillero vacio se representa con un punto (.)"""
+        self.board = []
+        for fila in xrange(9):
+            self.board.append([None for i in xrange(9)])
 
-    def poblar_celda(self, index):
-        if (index == 10):
-            return True
-        # probar con cada valor posible por celda
-        valores_posibles = [1,2,3,4,5,6,7,8,9]
-        # el shuffle es para que cada celda pruebe con los valores en distinto orden
-        random.shuffle(valores_posibles)
-        for i in xrange(len(valores_posibles)):
-            self.board[index] = valores_posibles[i]
-            if self.board_valido():
-                if self.poblar_celda(index+1):
-                    return True
-        self.board[index] = None    # rollback esta celda
-        return False
-
-    # Esta funcion se encarga de definir si el board asi como esta es valido.
-    # Para esto, recorre las 81 celdas, y por cada Ci verifica:
-    # 1) Que Ci no se repita en la misma columna
-    # 2) Que Ci no se repita en la misma fila
-    # 3) Que Ci no se repita dentro del mismo recuadro.
-    # Si estas condiciones son validas, se retorna un True.
-    def board_valido(self):
-    	for celda in xrange(len(self.board)):
-            if self.en_fila(celda) or self.en_columna(celda) or self.en_cuadro(celda):
-                return False
-        return True
-
-    # Para que se cumpla la condicion de que no esten en la misma fila, hay que verificar que:
-    # 1) filas iguales
-    # 2) datos iguales
-    # 3) haya datos
-    # index especifica la posicion del nuevo dato ingresado
-    # celda especifica la posicion del dato contra el cual hay que verificar
-    def en_fila(self, celda):
-        fila = int(celda/9)
-        for index in xrange(len(self.board)):
-            if int(index/9) == fila:
-                if celda != index:
-                    if self.board[index] == self.board[celda]:
-                        return True
-        return False
-
-    # Para que se cumpla la condicion de que no esten en la misma columna, hay que verificar que:
-    # 1) columnas iguales
-    # 2) datos iguales
-    # 3) haya datos
-    def en_columna(self, celda):
-        columna = celda % 9
-        for index in xrange(len(self.board)):
-            if index%9 == columna:
-                if celda != index:
-                    if self.board[celda] == self.board[index]:
-                        return True
-	return False
-
-    # Para que se cumpla la condicion de que no esten en el mismo recuadro, hay que verificar que:
-    # 1) recuadros iguales
-    # 2) datos iguales
-    # 3) haya datos
-    def en_cuadro(self, celda):
-        recuadro = self.cuadro(celda)
-        for index in xrange(len(self.board)):
-            if recuadro == self.cuadro(index):
-                if index != celda:
-                    if self.board[celda] == self.board[index]:
-                        return True
-        return False
-
-    def fila(self, celda):
-	return int(celda/9)
-		
-    def columna(self, celda):
-	return celda%9
-		
-    def cuadro(self, celda):
-	return (int(self.fila(celda)/3), self.columna(celda)%3)
-	
     def print_board(self):
-        print self.board
+        """Funcion que imprime el board sin los corchetes y por fila"""
+        for fila in self.board:
+            for item in fila:
+                print item,
+            print ""
+
+    def en_fila(self, fila, numero):
+        """Devuelve True si 'numero' se encuentra en la fila."""
+        if numero in self.board[fila] and numero != None:
+            return True
+        else:
+            return False
+
+    def en_columna(self, col, numero):
+        """Devuelve True si 'numero' se encuentra en la columna 'col'"""
+        for fila in xrange(9):
+            if self.board[fila][col] == numero and numero != None:
+                return True
+        return False
+
+    def en_cuadro(self, f_cuadro, c_cuadro, numero):
+        """Devuelve True si 'numero' se encuentra en el cuadro [f_cuadro, c_cuadro]"""
+        for fila in range(3 * f_cuadro, 3 * f_cuadro + 3):
+            for col in range(3 * c_cuadro, 3 * c_cuadro + 3):
+                if self.board[fila][col] == numero and numero != None:
+                    return True
+        return False
+
+#    def poblar_board(self, index):
+#        if index == 81:
+#            return True
+#
+#        valores_posibles = [1,2,3,4,5,6,7,8,9]
+#        random.shuffle(valores_posibles)
+#
+#        for i in xrange(9):
+#            self.board[int(index/9)][index%9] = valores_posibles[i]
+#            if (self.en_fila(int(index/9), valores_posibles[i]) == False) and (self.en_columna(index%9, valores_posibles[i]) == False) and (self.en_cuadro(int(round(int(index/9))/3), int(round((index%9)/3)), valores_posibles[i]) == False):
+#                if (self.poblar_board(index+1) == True):
+#                    return True
+#        self.board[int(index/9)][index%9] = None
+#        return False
+    def poblar_board(self):
+        """Esta funcion completa el tablero creando un sudoku con las reglas basicas (level-1)"""
+        random.seed()
+        col = 0
+        while col < 9:
+            valores_posibles = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            random.shuffle(valores_posibles)
+            fila = 0
+            prueba = 0
+            # Se prueban los valores por fila unas 200 veces
+            while fila < 9 and prueba < 200:
+                malas = 0
+                parar = 0
+                for elem in range(0, len(valores_posibles)):
+                    if (self.en_columna(col, valores_posibles[elem]) or self.en_fila(fila, valores_posibles[elem]) or self.en_cuadro(int(round(fila / 3)), int(round(col / 3)), valores_posibles[elem])):
+                        malas += 1
+                # Si llegue al maximo de malas (valores que quedan por probar), tengo que reiniciar
+                if malas == len(valores_posibles):
+                    # limpiar todos los valores de la columna
+                    for fila_t in xrange(9):
+                        self.board[fila_t][col] = None
+                    valores_posibles = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                    parar = 1
+                    fila = 0
+                    prueba += 1
+                if parar == 0:
+                    numero = random.randint(1, 9)
+                    while (self.en_columna(col, numero) or self.en_fila(fila, numero) or self.en_cuadro(int(round(fila / 3)), int(round(col / 3)), numero)):
+                        random.seed()
+                        numero = random.randint(1, 9)
+                    # El numero no se encuentra y cumple las condiciones, entonces asignar...
+                    self.board[fila][col] = numero
+                    # ... y quitar de valores_posibles para esa columna
+                    valores_posibles.remove(numero)
+                    fila += 1
+            col += 1
+            if prueba == 200:
+                self.limpiar_board()
+                col = 0
+
+#    def puzzle_solver(self):
+        # candidatos es una matriz triple con una lista de candidatos por conjunto de fila-columna.
+#        self.candidatos = [[[1,2,3,4,5,6,7,8,9] for i in xrange(9)] for j in xrange(9)]
+#        tiene_solucion = True
+#        while tiene_solucion ==  True:
+#            for fila in xrange(9):
+#                for col in xrange(9):
+#                    if len(self.candidatos[fila][col]) == 1:
+                        # si hay un unico valor para esa celda, entonces es seguro que va ahi, sacarlos de las demas celdas
+#                        self.board[fila][col] == int(self.candidatos[fila][col])
+#                        for fila_t in xrange(9):
+#                            self.candidatos[fila_t][col].remove(self.board[fila][col])
+#                        for col_t in xrange(9):
+#                            self.candidatos[fila][col_t].remove(self.board[fila][col])
+#        return
+
+#    def crear_puzzle(self, index):
+#        fila = random.randint(1,9)
+#        col = random.randint(1,9)
+
+#        while 
+
 
 
 if __name__ == "__main__":
-    mi_board = Sudoku(9)
-#    if mi_board.board_valido():
+    mi_board = Sudoku()
+    mi_board.poblar_board()
     mi_board.print_board()
